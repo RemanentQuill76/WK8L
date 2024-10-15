dc0db5a2e66c89239e62a951181ec0f7c968eb43
Tyler Ormstad <tormstad@c28-0112-24.ad.mtu.edu>
2024-10-15 13:50:08 -0400

    finished

--- HandleExceptions.java
+++ HandleExceptions.java
@@ -5,15 +5,23 @@ import java.util.Scanner;
 
 public class HandleExceptions {
     public static void main(String[] args) {
+	    int count = 0;
+	    double sum = 0.0;
         try {
             File file = new File("numbers.txt");
             Scanner scanner = new Scanner(file);
-            int sum = 0;
             while (scanner.hasNext()) {
-                sum += scanner.nextInt();
+		if(scanner.hasNextInt()) {
+		sum += (double) scanner.nextInt(); 
+		count++;
+		} else if (scanner.hasNextDouble()) {
+	        sum += scanner.nextDouble();
+		count++; //make	
+		}
             }
             System.out.println("Sum of numbers: " + sum);
             scanner.close();
+	    System.out.println("average is:" + sum/count);	
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
         } catch (InputMismatchException e) {
@@ -21,3 +29,4 @@ public class HandleExceptions {
         }
     }
 }
+//Modify - It throws the "File contains non-integer values"
